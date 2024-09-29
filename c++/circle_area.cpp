076316ca6d10df622581da2788eb376ff2924f01
Ethen <ethen.lcs09@gmail.com>
2024-09-29 11:39:45 +0700

    Add files via upload

---
+++ c++/circle_area.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+double A, n, R;
+
+n = 3.14159; 
+cout << "Enter radius";
+cin >> R;
+
+A = n*(R*R);
+
+cout << fixed << setprecision(3) << "Area = " << A << endl;
+
+return 0;
+}
